0a935bc4b18677a6b72756dcbbf083cce3313bb2
Alan Spadoni <aspadoni@groupon.com>
2017-08-21 17:45:12 -0500

    Fixed most rubocop suggestions

--- lib/spiralize.rb
+++ lib/spiralize.rb
@@ -1,35 +1,43 @@
 require 'matrix'
 
-module Spiralize
+module Spiralize # :nodoc:
   Matrix.include Spiralize
   def spiralize
     @matrix = self
-    return "" if @matrix.empty?
+    return '' if @matrix.empty?
 
-    @num_rows = @matrix.row_size-1
-    @num_columns = @matrix.column_size-1
-
-    @matrix_map = ((0..@num_rows).map{|i| (0..@num_columns).map{|j| [i, j]}}).flatten(1)
-
-    @result = ""
-    @positions_visited = []
-    @current_position = [0,0]
+    set_spiralize_data
 
     move_right
   end
 
   private
 
+  def set_spiralize_data
+    @num_rows = @matrix.row_size - 1
+    @num_columns = @matrix.column_size - 1
+
+    @matrix_map = (0..@num_rows).map do |i|
+      (0..@num_columns).map { |j| [i, j] }
+    end
+    @matrix_map.flatten!(1)
+    @result = ''
+    @positions_visited = []
+    @current_position = [0, 0]
+  end
+
   def move_right
     while @current_position[1] != @num_columns &&
-      !@positions_visited.include?([@current_position[0],(@current_position[1]+1)]) do
+          !@positions_visited.include?(
+            [@current_position[0], (@current_position[1] + 1)]
+          )
       mark_visited
       build_result
       @current_position[1] += 1
     end
 
     if at_end?
-      @result << "#{ @matrix[@current_position[0],@current_position[1]] }"
+      @result << "#{ @matrix[@current_position[0], @current_position[1]] }"
       return @result.downcase
     end
 
@@ -38,7 +46,9 @@ module Spiralize
 
   def move_down
     while @current_position[0] != @num_rows &&
-      !@positions_visited.include?([(@current_position[0]+1),@current_position[1]]) do
+          !@positions_visited.include?(
+            [(@current_position[0] + 1), @current_position[1]]
+          )
       mark_visited
       build_result
       @current_position[0] += 1
@@ -49,7 +59,9 @@ module Spiralize
 
   def move_left
     while @current_position[1] != 0 &&
-      !@positions_visited.include?([@current_position[0],(@current_position[1]-1)]) do
+          !@positions_visited.include?(
+            [@current_position[0], (@current_position[1] - 1)]
+          )
       mark_visited
       build_result
       @current_position[1] -= 1
@@ -60,7 +72,9 @@ module Spiralize
 
   def move_up
     while @current_position[0] != 0 &&
-      !@positions_visited.include?([(@current_position[0]-1),@current_position[1]]) do
+          !@positions_visited.include?(
+            [(@current_position[0] - 1), @current_position[1]]
+          )
       mark_visited
       build_result
       @current_position[0] -= 1
@@ -73,7 +87,7 @@ module Spiralize
   end
 
   def build_result
-    @result << "#{ @matrix[@current_position[0],@current_position[1]] } "
+    @result << "#{ @matrix[@current_position[0], @current_position[1]] } "
   end
 
   def at_end?
